b251433531eea00d1d43c830f9ce4c2d482159ef
b-konishi <konishi5282@gmail.com>
2022-10-04 16:42:21 +0900

    add abc266

---
+++ abc265/a.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+inline bool chkchar(string &s, char c) { return s.find(c) != string::npos; }
+inline void YESNO(bool c) { cout << (c ? "YES" : "NO") << endl; }
+inline void YesNo(bool c) { cout << (c ? "Yes" : "No") << endl; }
+inline void yesno(bool c) { cout << (c ? "yes" : "no") << endl; }
+template<class T> inline void pvec1(vector<T> &vec) { for (T _vec : vec) { cout << _vec << " "; } cout << endl; } //
+template<class T> inline void pvec2(vector<vector<T>> &vec) { for (T v : vec) { for (T h : v) { cout << h << " "; } cout << endl; }} //
+
+int main() {
+
+  // DEBUG
+  ifstream in("/home/konishi/workspace_atcoder/input.txt"); cin.rdbuf(in.rdbuf()); //
+
+  int X, Y, N;
+  cin >> X >> Y >> N;
+
+  int price = 3*X < Y ? N*X : (N%3)*X + (N/3)*Y;
+
+  cout << price << endl;
+}
